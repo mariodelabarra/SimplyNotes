6998d399c8b2e1ddf8f2af01d8a636e047442dde
Mario <48571002+mariodelabarra@users.noreply.github.com>
2020-09-28 21:58:38 -0300

    Shared component (#18)
    * Development (#17)
    
    * JWT for the Backend
    
    * Error handler
    
    * FrontEnd project
    
    * Bootstrap and Angular Material
    
    Co-authored-by: mariodelabarra <mariodelabarra>
    
    * Angular toolbar, material module and Board repository
    
    Co-authored-by: mariodelabarra <mariodelabarra>

--- BackEnd/SimplyNotes.DataAccess/SimplyNotesUnitOfWork.cs
+++ BackEnd/SimplyNotes.DataAccess/SimplyNotesUnitOfWork.cs
@@ -10,9 +10,11 @@ namespace SimplyNotes.DataAccess
             Note = new NoteRepository(connectionString);
             List = new ListRepository(connectionString);
             User = new UserRepository(connectionString);
+            Board = new BoardRepository(connectionString);
         }
         public INoteRepository Note { get; private set; }
         public IListRepository List { get; private set; }
         public IUserRepository User { get; private set; }
+        public IBoardRepository Board { get; set; }
     }
 }
---
+++ BackEnd/SimplyNotes.WebAPI/Controllers/BoardController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SimplyNotes.Models;
+using SimplyNotes.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimplyNotes.WebAPI.Controllers
+{
+    [Route("api/board")]
+    [Authorize]
+    public class BoardController: Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BoardController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_unitOfWork.Board.GetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Board board)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            return Ok(_unitOfWork.Board.Insert(board));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Board board)
+        {
+            if (ModelState.IsValid && _unitOfWork.Board.Update(board))
+            {
+                return Ok(new { Message = "The Board is Updated" });
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete([FromBody] Board board)
+        {
+            if (board.Id > 0)
+                return Ok(_unitOfWork.Board.Delete(board));
+            return BadRequest();
+        }
+    }
+}
